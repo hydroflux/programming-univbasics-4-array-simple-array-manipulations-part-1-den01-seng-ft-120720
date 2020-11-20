681b9bd07be941ff0180c6283b372621413b3b3c
Jack Hubert <jphubert93@gmail.com>
2020-11-20 22:59:39 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,14 +10,14 @@ def using_pop(array, string)
   array.pop(string)
 end
 
-def pop_with_args(array, string)
-
+def pop_with_args(array)
+  array.pop(2)
 end
 
 def using_shift(array, string)
   array.shift(string)
 end
 
-def shift_with_args(array, string)
-
+def shift_with_args(array)
+  array.shift(2)
 end
